c291b879030e8999e9814938f5990350e2666a51
nyanakumaro123 <nyanakumaro123@gmail.com>
2025-05-21 15:31:14 +0700

    Buat File

---
+++ MAD_ALP/MAD_ALP/MAD_ALPApp.swift
@@ -0,0 +1,17 @@
+//
+//  MAD_ALPApp.swift
+//  MAD_ALP
+//
+//  Created by Student on 21/05/25.
+//
+
+import SwiftUI
+
+@main
+struct MAD_ALPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
