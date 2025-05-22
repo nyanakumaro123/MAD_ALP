038b090b24d6cf593c973e3e9bd6ba040b42bcc9
Marcellas87 <meonggarong86862@outlook.com>
2025-05-22 12:28:53 +0700

    template and schedule add (unfinished one)

---
+++ MAD_ALP/MAD_ALP/MAD_ALP/Model/Schedule.swift
@@ -0,0 +1,18 @@
+//
+//  Schedule.swift
+//  MAD_ALP
+//
+//  Created by Student on 22/05/25.
+//
+
+import SwiftUI
+
+struct Schedule: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Schedule()
+}
---
+++ MAD_ALP/MAD_ALP/MAD_ALP/View/TemplateExerciseView.swift
@@ -0,0 +1,18 @@
+//
+//  TemplateExerciseView.swift
+//  MAD_ALP
+//
+//  Created by Student on 22/05/25.
+//
+
+import SwiftUI
+
+struct TemplateExerciseView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TemplateExerciseView()
+}
