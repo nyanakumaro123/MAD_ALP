0c9acc6ced3e845eb62545d931cb222ad825fe2a
nyanakumaro123 <nkisunyoto123@gmail.com>
2025-05-23 11:56:03 +0700

    perubahan lanjutan

---
+++ MAD_ALP/MAD_ALP/MAD_ALP/View/DetailExerciseView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailExerciseView.swift
+//  MAD_ALP
+//
+//  Created by student on 23/05/25.
+//
+
+import SwiftUI
+
+struct DetailExerciseView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailExerciseView()
+}
