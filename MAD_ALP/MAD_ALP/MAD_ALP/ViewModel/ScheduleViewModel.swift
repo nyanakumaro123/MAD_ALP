230dad7194a3ddaadba9987597b07491968c35c7
Marcellas87 <meonggarong86862@outlook.com>
2025-05-23 11:54:50 +0700

    add Exercise update v 1.1

---
+++ MAD_ALP/MAD_ALP/MAD_ALP/ViewModel/ScheduleViewModel.swift
@@ -0,0 +1,49 @@
+//
+//  HomeViewModel.swift
+//  MAD_ALP
+//
+//  Created by Student on 22/05/25.
+//
+
+import Foundation
+import WatchConnectivity
+//import SwiftData
+
+class ExerciseViewModel: NSObject, ObservableObject, WCSessionDelegate {
+    // Watch
+    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: (any Error)?) {
+        
+    }
+    
+    func sessionDidBecomeInactive(_ session: WCSession) {
+        
+    }
+    
+    func sessionDidDeactivate(_ session: WCSession) {
+        
+    }
+    // Watch
+    
+    // Init
+    @Published var exercises = [Exercise]()
+    var session: WCSession
+    
+    init(session: WCSession = .default) {
+        // Watch
+        self.session = session
+        
+        super.init()
+        session.delegate = self
+        session.activate( )
+    }
+    
+    func session(_ session: WCSession, didReceiveMessage message: [String: Any]) {
+        DispatchQueue.main.async {
+            print("Received message: \(message)")
+            
+//            self.books.append(Book(title: message["title"] as! String, author: message["author"] as! String, image: message["image"] as! String, genre: message["genre"] as! String))
+        }
+    }
+    
+    
+}
