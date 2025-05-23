cbdcc896abef1ca6292ea95a583d0b3f0e77030a
nyanakumaro123 <nkisunyoto123@gmail.com>
2025-05-23 11:18:43 +0700

    perubahan lanjutan

--- MAD_ALP/MAD_ALP/MAD_ALP/View/AddExerciseView.swift
+++ MAD_ALP/MAD_ALP/MAD_ALP/View/AddExerciseView.swift
@@ -6,16 +6,53 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct AddExerciseView: View {
+    @Environment(\.dismiss) var dismiss
+    @Environment(\.modelContext) private var modelContext
+    
+    // Data imput value
+    @State private var title = ""
+    @State private var selectedDate: Date = Date()
+    
+    init(title: String = "", date: Date = Date()) {
+        _title = State(initialValue: title)
+        _selectedDate = State(initialValue: date)
+    }
+    
     var body: some View {
-        VStack {
-            Text("Picked date")
-            Text("[|Image| OlahragaName ^]")
+        NavigationView {
+            Form {
+                Section(header: Text("gym exercise info")) {
+                    // Name
+                    TextField("Title", text: $title)
+                    
+                    DatePicker("Date", selection: $selectedDate, displayedComponents: .date)
+                }
+            }
+            .navigationTitle("Add Schedule")
+            .navigationBarItems(leading: Button("Cancel") {
+                dismiss()
+            })
+            .navigationBarItems(trailing: Button("Save") {
+//                viewModel.addFoodSpot(
+//                    name: name,
+//                    location: location,
+//                    description: description,
+//                    category: category,
+//                    visited: visited,
+//                    rating: rating,
+//                    userID: authViewModel.user?.uid,
+//                    context: modelContext
+//                )
+                dismiss()
+            })
         }
     }
 }
 
 #Preview {
     AddExerciseView()
+//    AddExerciseView(title: "Leg Day", date: ISO8601DateFormatter().date(from: "2021-04-16T00:00:00Z")!)
 }
--- MAD_ALP/MAD_ALP/MAD_ALP/ViewModel/SportViewModel.swift
+++
@@ -1,9 +0,0 @@
-//
-//  HomeViewModel.swift
-//  MAD_ALP
-//
-//  Created by Student on 22/05/25.
-//
-
-import Foundation
-
