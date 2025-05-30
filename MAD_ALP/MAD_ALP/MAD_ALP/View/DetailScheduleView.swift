9c586cda285823a80ffec1e041da4887e3e69854
Marcellas87 <meonggarong86862@outlook.com>
2025-05-30 12:28:03 +0700

    schedule card test

---
+++ MAD_ALP/MAD_ALP/MAD_ALP/View/DetailScheduleView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailScheduleView.swift
+//  MAD_ALP
+//
+//  Created by student on 30/05/25.
+//
+
+import SwiftUI
+
+struct DetailScheduleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailScheduleView()
+}
