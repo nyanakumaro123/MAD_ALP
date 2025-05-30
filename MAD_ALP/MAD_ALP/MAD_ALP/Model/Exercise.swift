2e52f84f4c349f445735f62e08f1804815d85e8a
nyanakumaro123 <nkisunyoto123@gmail.com>
2025-05-30 09:29:09 +0700

    pull kumaro

--- MAD_ALP/MAD_ALP/MAD_ALP/Model/Exercise.swift
+++ MAD_ALP/MAD_ALP/MAD_ALP/Model/Exercise.swift
@@ -13,10 +13,12 @@ class Exercise: Identifiable {
     var id: UUID
     var name: String
     var tips: [String]
+    var images: [String]
 
-    init(id: UUID = UUID(), name: String, tips: [String] = []) {
+    init(id: UUID = UUID(), name: String, tips: [String] = [], images: [String] = []) {
         self.id = id
         self.name = name
         self.tips = tips
+        self.images = images
     }
 }
