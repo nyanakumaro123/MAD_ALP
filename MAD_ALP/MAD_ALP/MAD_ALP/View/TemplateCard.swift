a89508f27228de4bc47db39fa840d0ae83ed5eda
Marcellas87 <meonggarong86862@outlook.com>
2025-06-04 16:31:05 +0700

    1/4 template card progress

--- MAD_ALP/MAD_ALP/MAD_ALP/View/TemplateCard.swift
+++ MAD_ALP/MAD_ALP/MAD_ALP/View/TemplateCard.swift
@@ -9,13 +9,10 @@ import SwiftUI
 import _SwiftData_SwiftUI
 
 struct TemplateCard: View {
-    var schedule: Schedule
+    var template: Template
     var index: Int
 
-    @State private var showDetail = false
-    
-    @State private var offsetX: CGFloat = 0
-    @GestureState private var dragOffset: CGFloat = 0
+    @State private var showEdit = false
     
     @Environment(\.modelContext) private var modelContext
     @Query var schedules: [Schedule]
@@ -40,96 +37,49 @@ struct TemplateCard: View {
 
     
     var body: some View {
-        ZStack(alignment: .trailing) {
-            // Background Pick button
-            HStack() {
-                Spacer()
-                Button {
-                    withAnimation {
-                        // go to edit
-                    }
-                } label: {
-                    Image(systemName: "pencil")
-                        .foregroundColor(.blue)
-                        .font(.system(size: 40))
-                }
-                .padding(.trailing, 20)
-                .cornerRadius(8)
-            }
+        VStack(alignment: .leading) {
+            Text("\(template.title)")
+                .font(.system(size: 24, weight: .bold, design: .rounded))
+                .foregroundStyle(textColorr)
+                .padding(.bottom, 4)
             
-            VStack(alignment: .leading) {
-                Text("\(schedule.title)")
-                    .font(.system(size: 24, weight: .bold, design: .rounded))
-                    .foregroundStyle(textColorr)
-                    .padding(.bottom, 4)
-                
-                Text("Date: \(schedule.date, style: .date) - \(schedule.time, style: .time)")
-                    .font(.system(size: 16, weight: .bold, design: .rounded))
-                    .foregroundStyle(textColorr)
-                    .padding(.bottom, 8)
-                
-                // scrollabe square exercise text name
-                ScrollView(.horizontal, showsIndicators: false) {
-                    HStack(spacing: 12) {
-                        ForEach(schedule.exercises, id: \.id) { exercise in
-                            VStack {
-                                Text(exercise.name)
-                                    .font(.system(size: 16, weight: .bold, design: .rounded))
-                                    .foregroundStyle(textColorr)
-                                    .multilineTextAlignment(.center)
-                                    .padding(8)
-                                    .frame(width: 80, height: 80)
-                                    .background(Color.white.opacity(0.8))
-                                    .cornerRadius(10)
-                            }
+            Text("\(template.descriptions)")
+                .font(.system(size: 16, weight: .bold, design: .rounded))
+                .foregroundStyle(textColorr)
+                .padding(.bottom, 8)
+            
+            // scrollabe square exercise text name
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(spacing: 12) {
+                    ForEach(template.exercises, id: \.id) { exercise in
+                        VStack {
+                            Text(exercise.name)
+                                .font(.system(size: 16, weight: .bold, design: .rounded))
+                                .foregroundStyle(textColorr)
+                                .multilineTextAlignment(.center)
+                                .padding(8)
+                                .frame(width: 80, height: 80)
+                                .background(Color.white.opacity(0.8))
+                                .cornerRadius(10)
                         }
                     }
                 }
             }
-            .padding(20)
-            .background(backgroundColor)
-            .offset(x: offsetX + dragOffset)
-            .onTapGesture {
-                if offsetX == 0 {
-                    withAnimation { showDetail = true }
-                }
-                else {
-                    withAnimation { offsetX = 0 }
-                }
-            }
-            
         }
+        .padding(20)
+        .background(backgroundColor)
         .frame(minWidth: 0, maxWidth: .infinity)
         .cornerRadius(12)
-        //.padding(12)
-            .gesture(
-                DragGesture()
-                    .updating($dragOffset) { value, state, _ in
-                        if value.translation.width < 0 {
-                            state = value.translation.width
-                        }
-                    }
-                    .onEnded { value in
-                        if value.translation.width < -100 {
-                            withAnimation {
-                                offsetX = -80
-                            }
-                        } else {
-                            withAnimation {
-                                offsetX = 0
-                            }
-                        }
-                    }
-            )
         .contentShape(Rectangle()) // Ensures whole card is tappable
-        .fullScreenCover(isPresented: $showDetail) {
-            DetailScheduleView(schedule: schedule)
+        .fullScreenCover(isPresented: $showEdit) {
+            AddScheduleView(
+                id: template.id,
+                title: template.title,
+                selectedDate: Date(),
+                time: Date(),
+                selectedExercises: template.exercises
+            )
         }
+        
     }
-}
-
-#Preview {
-    ScheduleCard(schedule: Schedule(title: "TitleName", date: Date(), time: Date(), exercises: [Exercise(name: "Push Up", tips: ["Keep back straight"]), Exercise(name: "Squat", tips: ["Bend knees", "Keep balance"])]), index: 0)
-    ScheduleCard(schedule: Schedule(title: "TitleName", date: Date(), time: Date(), exercises: [Exercise(name: "Push Up", tips: ["Keep back straight"]), Exercise(name: "Squat", tips: ["Bend knees", "Keep balance"])]), index: 1)
-    ScheduleCard(schedule: Schedule(title: "TitleName", date: Date(), time: Date(), exercises: [Exercise(name: "Push Up", tips: ["Keep back straight"]), Exercise(name: "Squat", tips: ["Bend knees", "Keep balance"])]), index: 2)
 }
